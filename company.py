082372273bc150d7452626dd0825e0c9cc913873
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2013-02-11 18:46:53 +0530

    Add boilerplate code

---
+++ company.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    company
+
+    Configuration of Avatax settings with the company
+
+    :copyright: © 2013 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
---
+++ setup.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+
+from setuptools import setup, Command
+import re
+import os
+import ConfigParser
+
+
+class XMLTests(Command):
+    """Runs the tests and save the result to an XML file
+
+    Running this requires unittest-xml-reporting which can
+    be installed using::
+
+        pip install unittest-xml-reporting
+
+    """
+    description = "Run tests with coverage and produce jUnit style report"
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import coverage
+        import xmlrunner
+        cov = coverage.coverage(source=["trytond.modules.avatax_calc"])
+        cov.start()
+        from tests import suite
+        xmlrunner.XMLTestRunner(output="xml-test-results").run(suite())
+        cov.stop()
+        cov.save()
+        cov.xml_report(outfile="coverage.xml")
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+config = ConfigParser.ConfigParser()
+config.readfp(open('tryton.cfg'))
+info = dict(config.items('tryton'))
+for key in ('depends', 'extras_depend', 'xml'):
+    if key in info:
+        info[key] = info[key].strip().splitlines()
+major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
+major_version = int(major_version)
+minor_version = int(minor_version)
+
+requires = []
+for dep in info.get('depends', []):
+    if not re.match(r'(ir|res|webdav)(\W|$)', dep):
+        requires.append('trytond_%s >= %s.%s, < %s.%s' %
+                (dep, major_version, minor_version, major_version,
+                    minor_version + 1))
+requires.append('trytond >= %s.%s, < %s.%s' %
+        (major_version, minor_version, major_version, minor_version + 1))
+
+setup(name='trytond_avatax_calc',
+    version=info.get('version', '0.0.1'),
+    description='Tryton module for tax rate calculation',
+    long_description=read('README.rst'),
+    author='Tryton',
+    url='http://www.tryton.org/',
+    download_url="http://downloads.tryton.org/" + \
+        info.get('version', '0.0.1').rsplit('.', 1)[0] + '/',
+    package_dir={'trytond.modules.avatax_calc': '.'},
+    packages=[
+        'trytond.modules.avatax_calc',
+        'trytond.modules.avatax_calc.tests',
+        ],
+    package_data={
+        'trytond.modules.avatax_calc': info.get('xml', []) \
+            + ['tryton.cfg', 'locale/*.po', 'icons/*.svg'],
+        },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Framework :: Tryton',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Legal Industry',
+        'Intended Audience :: Manufacturing',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Office/Business',
+        ],
+    license='GPL-3',
+    install_requires=requires,
+    zip_safe=False,
+    entry_points="""
+    [trytond.modules]
+    avatax_calc = trytond.modules.avatax_calc
+    """,
+    test_suite='tests',
+    test_loader='trytond.test_loader:Loader',
+    cmdclass={
+        'xmltests': XMLTests,
+    },
+    )
